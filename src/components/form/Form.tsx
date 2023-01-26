af6bdbe5fdd0ec72451031885817fba8f5225200
Robby Pambudi <5025211042@mhs.its.ac.id>
2023-01-26 20:39:50 +0700

    feat: upgrade smart from with subsrcriptions

--- src/components/form/Form.tsx
+++ src/components/form/Form.tsx
@@ -1,15 +1,18 @@
 import * as React from 'react';
 import {
+  DeepPartialSkipArrayKey,
   FieldValues,
   FormProvider,
   SubmitHandler,
   useForm,
   UseFormProps,
   UseFormReturn,
+  useWatch,
 } from 'react-hook-form';
 
 type FormProps<TFormValues extends FieldValues> = {
   onSubmit: SubmitHandler<TFormValues>;
+  setSubscriptions?: React.Dispatch<DeepPartialSkipArrayKey<FieldValues>>;
   children: (methods: UseFormReturn<TFormValues>) => React.ReactNode;
   className?: string;
 } & UseFormProps<TFormValues>;
@@ -18,9 +21,21 @@ const Form = <TFormValues extends Record<string, unknown>>({
   onSubmit,
   children,
   className,
+  setSubscriptions,
   ...rest
 }: FormProps<TFormValues>) => {
   const methods = useForm<TFormValues>(rest);
+
+  const subs = useWatch({
+    control: methods.control,
+  });
+
+  React.useMemo(() => {
+    function run() {
+      setSubscriptions?.(subs);
+    }
+    setSubscriptions && run();
+  }, [setSubscriptions, subs]);
 
   return (
     <div className={className}>
