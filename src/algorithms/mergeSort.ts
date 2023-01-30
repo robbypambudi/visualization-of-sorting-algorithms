5441005fe2a0442ad7e1f997b64392d70f1b33fd
Robby Pambudi <5025211042@mhs.its.ac.id>
2023-01-31 01:28:41 +0700

    fix: fixing marge sort algorithms

--- src/algorithms/mergeSort.ts
+++ src/algorithms/mergeSort.ts
@@ -1,46 +1,69 @@
 import { delay } from '@/lib/delay';
 
-const mergeSort = async (
-  array: number[],
+const merge = async (
+  data: number[],
+  left: number,
+  middle: number,
+  right: number,
   updateBars: (counter: number) => void
 ) => {
-  const merge = async (left: number[], right: number[]) => {
-    const result = [];
-    let indexLeft = 0;
-    let indexRight = 0;
+  let leftIndex = left;
+  let rightIndex = middle + 1;
+  let resultIndex = 0;
 
-    while (indexLeft < left.length && indexRight < right.length) {
-      if (left[indexLeft] < right[indexRight]) {
-        result.push(left[indexLeft]);
-        indexLeft++;
-      } else {
-        result.push(right[indexRight]);
-        indexRight++;
-      }
+  const result = new Array(right - left + 1);
+
+  // Merge the two subarrays  into one
+  while (leftIndex <= middle && rightIndex <= right) {
+    if (data[leftIndex] < data[rightIndex]) {
+      result[resultIndex++] = data[leftIndex++];
+    } else {
+      result[resultIndex++] = data[rightIndex++];
     }
-
-    return result.concat(left.slice(indexLeft)).concat(right.slice(indexRight));
-  };
-
-  const _mergeSort = async (array: number[]): Promise<number[]> => {
-    if (array.length <= 1) {
-      return array;
-    }
-
-    const middle = Math.floor(array.length / 2);
-    const left = array.slice(0, middle);
-    const right = array.slice(middle);
-
-    return merge(await _mergeSort(left), await _mergeSort(right));
-  };
-
-  const sortedArray = await _mergeSort(array);
-  for (let i = 0; i < sortedArray.length; i++) {
-    array[i] = sortedArray[i];
-    updateBars(i);
-    await delay(2);
   }
-  return array;
+
+  // if any add remaining elements from left array
+  while (leftIndex <= middle) {
+    result[resultIndex++] = data[leftIndex++];
+  }
+
+  // if any add remaining elements from right array
+  while (rightIndex <= right) {
+    result[resultIndex++] = data[rightIndex++];
+  }
+  // copy sorted elements back to orginal array
+  for (let i = 0; i < result.length; i++) {
+    data[left + i] = result[i];
+    updateBars(i);
+    await delay(1);
+  }
+};
+
+const mergeSortHelper = async (
+  data: number[],
+  left: number,
+  right: number,
+  updateBars: (counter: number) => void
+) => {
+  if (left === right) {
+    return;
+  }
+
+  const middle = Math.floor((left + right) / 2);
+  // recursively sort left and right subarrays
+  await Promise.all([
+    mergeSortHelper(data, left, middle, updateBars),
+    mergeSortHelper(data, middle + 1, right, updateBars),
+  ]);
+  // merge the two subarrays
+  await merge(data, left, middle, right, updateBars);
+};
+
+const mergeSort = async (
+  data: number[],
+  updateBars: (counter: number) => void
+) => {
+  await mergeSortHelper(data, 0, data.length - 1, updateBars);
 };
 
 export default mergeSort;
